e5e98049450909e85fd652b0eb4a37da672aed9c
Michelly Moreira <michelly.daiana@hotmail.com>
2023-06-01 19:35:10 -0300

    task 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -7,15 +7,11 @@ def stratification_range(period, range_time):
 def study_schedule(permanence_period, target_time):
     range_time = []
     for period in permanence_period:
-        # print(isinstance(period[0] , int))
-        # print(isinstance(period[1] , int))
-        # print(not(target_time))
-        if (type(period[0]) != int or type(period[1]) != int or not(target_time)):
+        if (type(period[0]) != int or
+            type(period[1]) != int or
+            not(target_time)):
             return None
-        else: stratification_range(period, range_time)
+        else:
+            stratification_range(period, range_time)
     return range_time.count(target_time)
     # raise NotImplementedError
-
-# permanence_period = [(2,2), (1,2), (2,3), (1,5), (4,5)]
-# target_time = []
-# print(study_schedule(permanence_period, target_time))
