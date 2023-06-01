70ad571012b04b13b36700c7dbf25f46a49b08d3
Michelly Moreira <michelly.daiana@hotmail.com>
2023-06-01 18:35:45 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,21 @@
+def stratification_range(period, range_time):
+    for hour in range(period[0], period[1]+1):
+        range_time.append(hour)
+    return range_time
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    range_time = []
+    for period in permanence_period:
+        # print(isinstance(period[0] , int))
+        # print(isinstance(period[1] , int))
+        # print(not(target_time))
+        if (type(period[0]) != int or type(period[1]) != int or not(target_time)):
+            return None
+        else: stratification_range(period, range_time)
+    return range_time.count(target_time)
+    # raise NotImplementedError
+
+# permanence_period = [(2,2), (1,2), (2,3), (1,5), (4,5)]
+# target_time = []
+# print(study_schedule(permanence_period, target_time))
