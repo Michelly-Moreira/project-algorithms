5cbc247776ac4b6559a4cc39b7aa8036952c47bc
Michelly Moreira <michelly.daiana@hotmail.com>
2023-06-02 03:36:59 -0300

    iniciando req 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,31 @@
+def insertion_sort(list_word):
+    size_string = len(list_word)
+    for letter in range(1, size_string):
+        key = list_word[letter]
+        new_position = letter -1
+        while new_position >= 0 and list_word[new_position] > key:# enquanto a chave for menor remaneja a letra pra frente
+            list_word[new_position + 1] = list_word[new_position]# remaneja a letra
+            new_position -= 1
+            list_word[new_position + 1] = key# insere a chave na posição correta
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not first_string or not second_string:# se a primeira ou a segunda string não existir
+        return (first_string, second_string, False)
+    
+    first_word = first_string.lower()# tornando todas as letras da primeira string em minúsculas
+    # print(first_word)
+    second_word = second_string.lower()# tornando todas as letras da segunda string em minúsculas
+    # print(second_word)
+
+    insertion_sort(first_word)
+    insertion_sort(second_word)
+
+    if first_word == second_word:
+        return (first_word, second_word, True)
+    else:
+        return (first_word, second_word, False)
+
+    #raise NotImplementedError
+
+# print(is_anagram('Perda', 'Pedra'))
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,7 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if not word:
         return False
-    each_letter = [*word]
+    each_letter = [*word]# separando cada letra da palavra
 
     if each_letter[low_index] == each_letter[high_index] and high_index != 0:
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
