73ad7cbf67b22bea8fbad8d8033398daab917a60
Michelly Moreira <michelly.daiana@hotmail.com>
2023-06-01 23:04:58 -0300

    task 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,16 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    each_letter = [*word]
+    low_index += 1
+    inverted_letters = each_letter[::-1]
+
+    if not word:
+        return False
+    elif low_index >= high_index:
+        return True
+    elif each_letter == inverted_letters:
+        return True and is_palindrome_recursive(word, low_index, high_index)
+    else:
+        return False
+    # raise NotImplementedError
+
+print(is_palindrome_recursive('', 0, -1))
